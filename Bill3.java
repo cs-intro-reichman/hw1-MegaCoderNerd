7e0ee9a8483f8820a5f38fba66738278cf565903
Omer <godmega67@gmail.com>
2024-11-04 19:05:54 +0200

    Successfully finished the homework

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,14 @@
 // Splits a restaurant bill evenly among three diners.
+
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		// I used ceiling method to round up the bill division to the nearest integer
+		// I divided an integer by a double in order for the product to be a double
+		double billPerPerson =  Math.ceil(Integer.parseInt(args[3]) / 3.0);
+	    System.out.println("Dear " + name1 +", "+ name2 + " and " +name3+": pay " + 
+			billPerPerson + " ILS each");
 	}
 }
